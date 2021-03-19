bf7c584cd91deac6e0b53f6dd98298944630f513
dengninghai <2640559435@qq.com>
2021-03-19 17:59:04 +0800

    update change mode to light

--- source-src/js/changelight.js
+++ source-src/js/changelight.js
@@ -7,10 +7,34 @@ function init() {
   const intrudeLess = document.querySelector('#intrude-less');
 
   function toggleLight() {
-    container.classList.toggle('container')
-    toolsCol.classList.toggle('lightToolsCol')
-    btnctn.classList.toggle('lightBtnctn')
-    intrudeLess.classList.toggle('lightIntrudeLess')
+    if (container.getAttribute('class') === 'container') {
+      container.classList.remove('container');
+      sessionStorage.removeItem('container');
+    } else {
+      container.classList.add('container');
+      sessionStorage.setItem('container', true);
+    }
+    if (toolsCol.getAttribute('class').includes('lightToolsCol')) {
+      toolsCol.classList.remove('lightToolsCol');
+      sessionStorage.removeItem('lightToolsCol');
+    } else {
+      toolsCol.classList.add('lightToolsCol');
+      sessionStorage.setItem('lightToolsCol', true);
+    }
+    if (btnctn.getAttribute('class').includes('lightBtnctn')) {
+      btnctn.classList.remove('lightBtnctn');
+      sessionStorage.removeItem('lightBtnctn');
+    } else {
+      btnctn.classList.add('lightBtnctn');
+      sessionStorage.setItem('lightBtnctn', true);
+    }
+    if (intrudeLess.getAttribute('class').includes('lightIntrudeLess')) {
+      intrudeLess.classList.remove('lightIntrudeLess');
+      sessionStorage.removeItem('lightIntrudeLess');
+    } else {
+      intrudeLess.classList.add('lightIntrudeLess');
+      sessionStorage.setItem('lightIntrudeLess', true);
+    }
   }
   // PC端头像
   profilepic.onclick = function () {
@@ -19,6 +43,27 @@ function init() {
   // 移动端头像
   mobileProfilepic.onclick = function () {
     toggleLight();
+  }
+
+  if (sessionStorage.getItem('container')) {
+    container.classList.add('container');
+  } else {
+    container.classList.remove('container');
+  }
+  if (sessionStorage.getItem('lightBtnctn')) {
+    btnctn.classList.add('lightBtnctn');
+  } else {
+    btnctn.classList.remove('lightBtnctn');
+  }
+  if (sessionStorage.getItem('lightToolsCol')) {
+    toolsCol.classList.add('lightToolsCol');
+  } else {
+    toolsCol.classList.remove('lightToolsCol');
+  }
+  if (sessionStorage.getItem('lightIntrudeLess')) {
+    intrudeLess.classList.add('lightIntrudeLess');
+  } else {
+    intrudeLess.classList.remove('lightIntrudeLess');
   }
 }
 
