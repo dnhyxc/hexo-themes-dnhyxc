95e93074a53c7fbc4b434585b5618aa9a82e75a4
dengninghai <2640559435@qq.com>
2021-04-04 00:08:57 +0800

    change tags styles

--- source-src/js/setmargin.js
+++ source-src/js/setmargin.js
@@ -1,11 +1,12 @@
 function init() {
-  let path = location.pathname;
-  const archivesWrap = document.querySelector('.archives-wrap');
-  const wh = window.screen.height;
-  const bh = document.body.clientHeight;
+  const path = location.pathname;
   const res = decodeURIComponent(path).substr(decodeURIComponent(path));
-  if (res.includes('tags') || res.includes('categories') && document.body.clientWidth <= 800) {
-    archivesWrap.style.marginBottom = (wh - bh + 10) + 'px';
+  const jsContent = document.querySelector('#js-content');
+
+  if ((res.includes('tags') || (res.includes('categories') && ((res.split('/')).length - 1) > 2)) && document.body.clientWidth <= 800) {
+    if (jsContent.childElementCount < 2) {
+      document.querySelector('#container').style.height = '100vh';
+    }
   }
 
   const mobileDark = document.querySelector('.mobileDark');
@@ -13,7 +14,6 @@ function init() {
 
   let timer;
   document.body.onscroll = function () {
-    console.log(document.documentElement.scrollTop);
     mobileDark.style.visibility = 'visible';
     mobileDark.style.transition = 'all 0.5s';
     toTopDark.style.visibility = 'visible';
